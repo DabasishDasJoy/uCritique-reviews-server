a7b62570736b53adc73216d22abac8246b516331
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-11-08 01:11:53 +0600

    database connected

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-/*
+/*---------------------
     MiddleWare
     1: Cors
     2: Body parser
-*/
+-----------------------*/
 app.use(cors());
 app.use(express.json());
+
+/* --------Middleware end-------- */
+
+/* --------------root route------------ */
 
 app.get("/", (req, res) => {
   console.log(req.method);
   res.json({ message: "Success", data: "Server is running" });
 });
-app.get("/test", (req, res) => {
-  console.log(req.method);
-  res.json({ message: "Success", data: `Test ${process.env.TEST} recieved` });
+/* --------------root route end------------ */
+
+/* ------------Database Connection---------- */
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.mj0nqa8.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 app.listen(port, () => {
+  client.connect((err) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("DB connected");
+    }
+  });
+
   console.log("App is running on port: ", port);
 });
