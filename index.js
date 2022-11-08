7518b105e25422e44e3c5c97dbdb0278519e23b9
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-11-08 14:35:59 +0600

    updated require

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const {
-  MongoClient,
-  ServerApiVersion,
-  ObjectId,
-  ObjectID,
-} = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const verifyJWT = require("./middleware/verifyJWT");
 
